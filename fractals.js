0e8d673f97c22a760b84af224f43772359a0e6f3
nony-1205 <nonymeko2008@gmail.com>
2026-06-02 11:14:21 +0000

    Jun 2, 2026, 2:14 PM

--- fractals.js
+++ fractals.js
@@ -177,7 +177,7 @@ window.addEventListener('load',() =>{
             const re= (e.clientX - mandelbrot.canvas.offsetLeft - mandelbrot.canvas.width/2) / (zoom * mandelbrot.canvas.height*0.5) +offsetX
             const im= (e.clientY - mandelbrot.canvas.offsetTop -mandelbrot.canvas.height/2) / (zoom * mandelbrot.canvas.height*0.5) - offsetY
 
-            console.log('re:', re, 'im:', im)
+            // console.log('re:', re, 'im:', im)
 
             julia.gl.useProgram(julia.program)
             julia.gl.uniform2f(julia.uJulia, re, im)
