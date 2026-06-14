bd732c123b6c9ffad2d9c1a27bbeaf75ab75360b
nony-1205 <nonymeko2008@gmail.com>
2026-06-14 16:43:10 +0000

    Jun 14, 2026, 7:43 PM

--- fractals.js
+++ fractals.js
@@ -130,9 +130,9 @@ void main(){
         float t= (escapeIter< maxIter)
             ? (float(escapeIter) - log2(log2(max(dot(z_single,z_single), 0.1))) +4.0) / float(maxIter)
             : 0.0;
-        float r = 0.5 + 0.5*cos(6.28318 *(3.0 * t + 0.30 +  u_time));
-        float g = 0.5 + 0.5*cos(6.28318 * (3.0 * t + 0.23 + u_time));
-        float b = 0.5 + 0.5*cos(6.28318 * (3.0 * t + 0.87 + u_time));
+        float r = 0.5 + 0.5*cos(6.28318 *(6.0 * t + 0.30 +  u_time));
+        float g = 0.5 + 0.5*cos(6.28318 * (6.0 * t + 0.23 + u_time));
+        float b = 0.5 + 0.5*cos(6.28318 * (6.0 * t + 0.87 + u_time));
 
         if (escapeIter==maxIter){
             gl_FragColor= vec4(0.0, 0.0, 0.0, 1.0); //pure black
@@ -177,9 +177,9 @@ void main(){
         float t= (escapeIter< maxIter)
             ? (float(escapeIter) - log2(log2(dot(z,z))) +4.0) / float(maxIter)
             : 0.0;
-        float r = 0.5 + 0.5*cos(6.28318 *(3.0 * t + 0.30 +  u_time));
-        float g = 0.5 + 0.5*cos(6.28318 * (3.0 * t + 0.23 + u_time));
-        float b = 0.5 + 0.5*cos(6.28318 * (3.0 * t + 0.87 + u_time));
+        float r = 0.5 + 0.5*cos(6.28318 *(6.0 * t + 0.30 +  u_time));
+        float g = 0.5 + 0.5*cos(6.28318 * (6.0 * t + 0.23 + u_time));
+        float b = 0.5 + 0.5*cos(6.28318 * (6.0 * t + 0.87 + u_time));
 
         if (escapeIter==maxIter){
             gl_FragColor= vec4(0.0, 0.0, 0.0, 1.0); //pure black
